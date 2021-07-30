2134e49e7d908ac618345f9e6173e820890abbb6
Ryan Peden <ryan04@gmail.com>
2021-07-30 09:20:01 -0400

    updated App component to include map, text box, and button

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { WebView } from 'react-native-webview';
+import mapTemplate from './map-template';
+
 
 export default function App() {
+  let webRef = undefined;
+  let [mapCenter, setMapCenter] = useState('-121.913, 37.361');
+  const run = `
+      document.body.style.backgroundColor = 'blue';
+      true;
+    `;
+
+  const onButtonClick = () => {
+    const [lng, lat] = mapCenter.split(",");
+    webRef.injectJavaScript(`map.setCenter([${parseFloat(lng)}, ${parseFloat(lat)}])`);
+  }
+
+  const handleMapEvent = (event) => {
+    setMapCenter(event.nativeEvent.data)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.buttons}>
+        <TextInput 
+        style={styles.textInput}
+        onChangeText={setMapCenter}
+        value={mapCenter}></TextInput>
+        <Button title="Set Center" onPress={onButtonClick}></Button>
+      </View>
+      <WebView
+        ref={(r) => (webRef = r)}
+        onMessage={handleMapEvent}
+        style={styles.map}
+        originWhitelist={['*']}
+        source={{ html: mapTemplate }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'column',
+    flex: 1
+  },
+  buttons: {
+    flexDirection: 'row',
+    height: '15%',
     backgroundColor: '#fff',
+    color: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 12
+  },
+  textInput: {
+    height: 40,
+    width: "60%",
+    marginRight: 12,
+    paddingLeft: 5,
+    borderWidth: 1,
+  },
+  map: {
+    width: '100%',
+    height: '85%',
     alignItems: 'center',
     justifyContent: 'center',
   },
